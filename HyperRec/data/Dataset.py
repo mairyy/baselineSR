d7aa4c6e0919219d7b7548deca55d1dc166cfa82
mairyy <quynhmai2193@gmail.com>
2024-02-20 20:36:31 +0700

    HyperRec

--- HyperRec/data/Dataset.py
+++ HyperRec/data/Dataset.py
@@ -146,16 +146,16 @@ def data_partition_neg(args):
             if(u <= 22363):
                 neg_test[u].append(i)
     sequences = np.zeros((usernum + 1, 101),dtype=np.int64)
-    print(sequences, type(sequences), len(sequences))
     for user in range(1, usernum+1):
         sequences[user][:] = neg_test[user]
-        print(neg_test[user])
+        print(type(neg_test[user]))
     # neg_test = list(np.arange(1, itemnum+1))
     # sequences = np.zeros((usernum + 1, usernum+1),dtype=np.int64)
     # for user in range(1, usernum+1):
     #     sequences[user][:] = neg_test
-    print(sequences, type(sequences))
+
     neg_test = sequences.copy()
+    print(type(neg_test))
     print('u i', usernum, itemnum)
     return [user_train, user_valid, user_train_valid, user_test, (user_train_time, user_valid_time, \
         user_train_valid_time, user_test_time, time_set_train, time_set_test), neg_test, itemnum+1, usernum+1]
