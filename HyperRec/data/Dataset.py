ac54b812c6051757dca6a5407a4858be5d5fe5dc
mairyy <quynhmai2193@gmail.com>
2024-02-20 22:46:27 +0700

    HyperRec

--- HyperRec/data/Dataset.py
+++ HyperRec/data/Dataset.py
@@ -10,6 +10,33 @@ import operator
 
 data_path = 'data/'
 
+def filter_k_core_inters(inters, user_inter_threshold=5, item_inter_threshold=5):
+    print(f"Filter K core: user {user_inter_threshold}, item {item_inter_threshold}")
+    while True:
+        user_count = {}
+        item_count = {}
+        for inter in inters:
+            if inter[0] not in user_count:
+                user_count[inter[0]] = 1
+            else:
+                user_count[inter[0]] += 1
+
+            if inter[1] not in item_count:
+                item_count[inter[1]] = 1
+            else:
+                item_count[inter[1]] += 1
+
+        new_inters = []
+        for inter in inters:
+            if user_count[inter[0]] >= user_inter_threshold and \
+                    item_count[inter[1]] >= item_inter_threshold:
+                new_inters.append(inter)
+
+        print(f"\tFilter: {len(inters)} inters to {len(new_inters)} inters")
+        if len(new_inters) == len(inters):
+            return new_inters
+        inters = new_inters
+
 def data_partition_neg(args):
     usernum = 0
     itemnum = 0
@@ -29,7 +56,25 @@ def data_partition_neg(args):
     user_train_valid_time = {}
     user_test_time = {}
     # assume user/item index starting from 1
-    path_to_data = data_path + args.data + '/' + 'all_seq_with_time'
+    path_to_data = data_path + args.data + '/' + 'ratings_Beauty.csv'
+    inters = []
+    f = open(path_to_data, 'r')
+    for uid, line in enumerate(f):
+        u, i, r, t = line.rstrip().split(',')
+        inters.append([u, i, t])
+    new_iters = filter_k_core_inters(inters)
+    uids, iids = {}, {}
+    u, i = 1, 1
+    for line in new_iters:
+        if line[0] not in uids:
+            uids[line[0]] = u
+            u += 1
+        if line[1] not in iids:
+            iids[line[1]] = i
+            i += 1
+        ids_iter = []
+    for line in new_iters:
+        ids_iter.append([uids[line[0]], iids[line[1]], line[2]])
 
     # for Amazon    
     t_map = {1997:[1], 1998:[1], 1999:[1], 2000:[1], 2001:[1], 2002:[1], 2003:[1], 2004:[1], 2005:[1], 2006:[1], 2007:[1], 2008:[1], 2009:[2,3], 2010:[4,5], \
@@ -38,9 +83,10 @@ def data_partition_neg(args):
      
     m_map = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:1, 8:1, 9:1, 10:1, 11:1, 12:1}
 
-    with open(path_to_data, 'rb') as out:
-        f = pickle.load(out)
+    # with open(path_to_data, 'rb') as out:
+    #     f = pickle.load(out)
     # f = open(path_to_data, 'r')
+    f = ids_iter
     for line in f:
         [u, i, t] = line
         # u, i, t, d = line.rstrip().split('\t')
