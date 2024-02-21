a24ed2f4759d7df0bddb6acd092028dc39dd03b2
mairyy <quynhmai2193@gmail.com>
2024-02-21 15:38:07 +0700

    Bert4Rec

--- BERT4Rec/modeling.py
+++ BERT4Rec/modeling.py
@@ -354,11 +354,13 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
     """Run layer normalization on the last dimension of the tensor."""
-    return tf.keras.layers.LayerNormalization(
-        inputs=input_tensor,
-        begin_norm_axis=-1,
-        begin_params_axis=-1,
-        scope=name)
+    layer_norm = tf.keras.layers.LayerNormalization(axis=-1, name=name)
+    return layer_norm(input_tensor)
+    # return tf.contrib.layers.layer_norm(
+    #     inputs=input_tensor,
+    #     begin_norm_axis=-1,
+    #     begin_params_axis=-1,
+    #     scope=name)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
